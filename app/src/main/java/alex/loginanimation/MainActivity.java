8600e544b4f1a1cb872bbaea6030ab895c880069
Nexen23 <tsalexn@mail.ru>
2017-06-20 21:08:35 +0700

    added reveal animation

--- app/src/main/java/alex/loginanimation/MainActivity.java
+++ app/src/main/java/alex/loginanimation/MainActivity.java
@@ -1,15 +1,18 @@
 package alex.loginanimation;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 	@BindView(R.id.b_register)
@@ -32,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
 		//loginCardView.setClipToOutline(false); // controls anyway have not onClick received
 	}
 
-	// HACK: plays hiding animation. Better use theme
+	@OnClick(R.id.b_register)
+	void onRegisterClick() {
+		View animatingView = loginCardView;
+		int startRadius = registerButton.getWidth();
+		int endRadius = Math.max(loginCardView.getWidth(), loginCardView.getHeight());
+		int x = (int) (loginCardView.getWidth() * 0.9f);
+		int y = loginCardView.getHeight() / 2;
+
+		Animator animator = ViewAnimationUtils.createCircularReveal(animatingView, x, y, startRadius, endRadius);
+		animator
+				.setDuration(1000)
+				.start();
+	}
+
 	void hideStatusBar() {
+		// HACK: plays hiding animation. Better use theme
 		View decorView = getWindow().getDecorView();
 		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
 		decorView.setSystemUiVisibility(uiOptions);
